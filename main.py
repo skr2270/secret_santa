642b748f482f8b086cc3bdca0f564f392b15f1a6
Sai Kumar Rayavarapu <sk.r.2270@gmail.com>
2025-07-18 11:40:23 +0530

    Placed provided data files inside data directory

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ from utils.file_handler import read_employee_list, read_previous_assignments, wr
 from services.santa_assigner import SecretSantaAssigner
 
 def main():
-    employees = read_employee_list('Employee-List.xlsx')
-    previous_assignments = read_previous_assignments('Secret-Santa-Game-Result-2023.xlsx')
+    employees = read_employee_list('data/Employee-List.xlsx')
+    previous_assignments = read_previous_assignments('data/Secret-Santa-Game-Result-2023.xlsx')
 
     assigner = SecretSantaAssigner(employees, previous_assignments)
     assignments = assigner.assign_secret_children()
 
-    write_assignments_to_csv(assignments, 'New_Secret_Santa_Assignments.csv')
-    print("Secret Santa assignments saved to 'New_Secret_Santa_Assignments.csv'.")
+    write_assignments_to_csv(assignments, 'data/New_Secret_Santa_Assignments.csv')
+    print("Secret Santa assignments saved to 'data/New_Secret_Santa_Assignments.csv'.")
 
 if __name__ == "__main__":
     main()
